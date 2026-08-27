5e4c684d868d486b4ceb905ad3e1fafc46e4b305
Vegujoal <alejandrov5414@gmail.com>
2026-08-27 13:58:13 -0400

    Commit Exp 1

--- direcciones.cpp
+++ direcciones.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 
 int main() {
-    cout << "Lab 2 - Pointers" << endl;
-    cout << "This program demonstrates the use of pointers in C++." << endl;
+    int num = 25;
+
+    // Dirección de memoria
+    cout << "Direccion de num: " << &num << endl;
+
+    // Declarar y asignar un pointer
+    int *ptr = nullptr;
+    ptr = &num;
+
+    cout << "ptr apunta a: " << ptr << endl;
+    cout << "Valor apuntado (*ptr): " << *ptr << endl;
+
+    // Modificar el valor original a traves del pointer
+    *ptr = 100;
+    cout << "num despues de *ptr = 100: " << num << endl;
+    
+    //Ejercicio numero 1:
+    double precio = 19.99;
+    double *pptr = &precio;
+    cout << "Direccion de precio: " << &precio << endl;
+    cout << "pptr apunta a: " << pptr << endl;
+    cout << "Valor apuntado (*pptr): " << *pptr << endl;
+    
     return 0;
 }
